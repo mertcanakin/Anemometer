264fd134f75eacbe313f7ac7e977b2e7b3e27aa3
Mertcan Akin <mertcanakin95@gmail.com>
2020-06-26 13:41:03 +0300

    Hız kodu eklendi.Bug var.

--- direction_speed/direction_speed.ino
+++ direction_speed/direction_speed.ino
@@ -1,3 +1,4 @@
+// Wind Direction Part:
 #define outputA 8
 #define outputB 9
 #define button 7
@@ -7,16 +8,46 @@ int a_state;
 int a_lastState;  
 int button_state = 0;
 
+// Wind Speed Part:
+int sensor = A0;
+const float wid = 0.04;   //adjust the width of the fan blade 
+const float rad = 0.01;   //adjust the radius of the point of detection
+const float pi = 3.14;  
+float time1;
+float time2;
+float vel;
+float diff;
+float tnet;
+float rpm;
+
 void setup() { 
   pinMode(outputA,INPUT);
   pinMode(outputB,INPUT);
   pinMode(button, INPUT);
-   
+  pinMode(sensor,INPUT);
+
   Serial.begin(9600);
   // initial state of the outputA
   a_lastState = digitalRead(outputA);   
 } 
 void loop() { 
+
+  // Wind Speed Calculation:
+  if(analogRead(sensor)<980)
+  {
+    time1 = millis();
+    delay(30);
+  }
+  if(analogRead(sensor)>980)
+  {
+    time2 = millis();
+    diff = (time2-time1);         
+    vel = wid/diff;                      //rotation velocity
+    tnet = (2*pi*rad)/vel;              //time = (2*pi*radius)/velocity.
+    rpm = (60000)/tnet;                // time in ms to minutes and then to rpm conversion step.
+  }
+
+  // Wind Direction Calculation:
   a_state = digitalRead(outputA); // Current state of the outputA
   button_state = digitalRead(button);
   
@@ -28,42 +59,41 @@ void loop() {
     } else {
       counter --;
     }
-    direction();
+
+
   } 
 
   // Calibration
   if(button_state == 1){
     counter = 0;
   }
-
   a_lastState = a_state; // Updates the previous state of the outputA with the current state
+    
 }
-
 void direction(){
   int mapped = counter * 12 % 360;
-
   if((mapped >= -24 && mapped <= 0) || (mapped <= -336 && mapped >= -359) || (mapped >= 0 && mapped <=24))  {
-    Serial.println("N");
+    Serial.print("N");
   }
   if((mapped >24 && mapped <= 72) || (mapped > -336 && mapped <= -288)){
-    Serial.println("N-E");
+    Serial.print("N-E");
   }
   if((mapped > 72 && mapped <= 108) || (mapped > -288 && mapped <= -252)){
-    Serial.println("E");
+    Serial.print("E");
   }
   if((mapped > 108 && mapped <= 156) || (mapped > -252 && mapped <= -216)){
-    Serial.println("S-E");
+    Serial.print("S-E");
   }
   if((mapped > 156 && mapped <= 204) || (mapped > -216 && mapped <= -168)){
-    Serial.println("S");
+    Serial.print("S");
   }
   if((mapped > 204 && mapped <= 264) || (mapped > -168 && mapped <= -120)){
-    Serial.println("S-W");
+    Serial.print("S-W");
   }
   if((mapped > 264 && mapped <= 300) || (mapped > -120 && mapped <= -72)){
-    Serial.println("W");
+    Serial.print("W");
   }
   if((mapped > 300 && mapped <= 359) || (mapped > -72 && mapped <= -25)){
-    Serial.println("N-W");
+    Serial.print("N-W");
   }
 }
