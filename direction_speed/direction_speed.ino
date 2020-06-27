9b740796079766860af6c6cecf80921aabcfab0d
Mertcan Akin <mertcanakin95@gmail.com>
2020-06-27 19:49:18 +0300

    Hiz, fonksiyon haline getirildi. Duzenlemeler yapildi.

--- direction_speed/direction_speed.ino
+++ direction_speed/direction_speed.ino
@@ -9,14 +9,16 @@ int a_lastState;
 int button_state = 0;
 String dir;
 int mapped;
+
 // Wind Speed Part:
 int sensor = A0;
-  
 float time1;
 float time2;
-float vel;
 float diff;
 float wind_speed;
+const float r = 0.0001; //10 cm to km
+const float pi = 3.1415;
+
 void setup() { 
   pinMode(outputA,INPUT);
   pinMode(outputB,INPUT);
@@ -28,20 +30,6 @@ void setup() {
   a_lastState = digitalRead(outputA);   
 } 
 void loop() { 
-
-  // Wind Speed Calculation:
-  if(analogRead(sensor)<980)
-  {
-    time1 = millis();
-    delay(30);
-  }
-  if(analogRead(sensor)>980)
-  {
-    time2 = millis();
-    diff = (time2-time1); 
-    diff = diff / 1000;
-    wind_speed = (2*3.14*0.0001*3600) / diff;         
-  }
 
   // Wind Direction Calculation:
   a_state = digitalRead(outputA); // Current state of the outputA
@@ -55,23 +43,74 @@ void loop() {
     } else {
       counter --;
     }
-    
+   mapped = counter * 12 % 360; 
    } 
+  
 
   // Calibration
   if(button_state == 1){
     counter = 0;
   }
-  
-  a_lastState = a_state; // Updates the previous state of the outputA with the current state
   direction();
+  windspeed();    
+
+  a_lastState = a_state; // Updates the previous state of the outputA with the current state
+
   Serial.print(dir);
   Serial.print("  ***  ");
-  Serial.println(wind_speed);
+  Serial.println(wind_speed);  
+  
+  
+}
+
+
+void windspeed(){
+
+  // Wind Speed Calculation:
+  if(analogRead(sensor)<980)
+  {
+    time1 = millis();
+    delay(30);
+  }
+  if(analogRead(sensor)>980)
+  {
+    time2 = millis();
+    diff = (time2-time1); 
+    diff = diff / 1000;
+    wind_speed = (2 * pi * r * 3600)/diff;    
+  }
   
 }
 void direction(){
-  mapped = counter * 12 % 360;
+/*
+  //mapped = counter % 30;   
+
+  if((mapped >= -1 && mapped <= 0) || (mapped <= -1&& mapped >= -28) || (mapped >= 0 && mapped <=2) || (mapped >27 && mapped <=30))  {
+    dir = "N";
+  }
+  if((mapped >2 && mapped <= 6) || (mapped > -28 && mapped <= -23)){
+    dir = "N-E";
+  }
+  if((mapped > 6 && mapped <= 9) || (mapped > -23 && mapped <= -20)){
+    dir = "E";
+  }
+  if((mapped > 9 && mapped <= 13) || (mapped > -20 && mapped <= -16)){
+    dir = "S-E";
+  }
+  if((mapped > 13 && mapped <= 16) || (mapped > -15 && mapped <= -13)){
+    dir = "S";
+  }
+  if((mapped > 16 && mapped <= 21) || (mapped > -13 && mapped <= -8)){
+    dir = "S-W";
+  }
+  if((mapped > 21 && mapped <= 24) || (mapped > -8 && mapped <= -5)){
+    dir = "W";
+  }
+  if((mapped > 24 && mapped <= 27) || (mapped > -5 && mapped <= -2)){
+    dir = "N-W";
+  }
+*/
+  
   if((mapped >= -24 && mapped <= 0) || (mapped <= -336 && mapped >= -359) || (mapped >= 0 && mapped <=24))  {
     dir = "N";
   }
@@ -96,4 +135,5 @@ void direction(){
   if((mapped > 300 && mapped <= 359) || (mapped > -72 && mapped <= -25)){
     dir = "N-W";
   }
+
 }
