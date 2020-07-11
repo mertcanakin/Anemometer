f8f1f5eb58a05cbc9598137407a1a6845e151b61
Mertcan Akin <mertcanakin95@gmail.com>
2020-07-12 02:38:21 +0300

    km/h eklendi.

--- direction_speed/direction_speed.ino
+++ direction_speed/direction_speed.ino
@@ -11,19 +11,26 @@ String dir;
 int mapped;
 
 // Wind Speed Part:
-int sensor = A0;
-float time1;
-float time2;
-float diff;
-float wind_speed;
-const float r = 0.0001; //10 cm to km
+int sensor = 2;
+float value = 0;
+float rev = 0;
+int rpm;
+int oldtime = 0;
+int time;
+float wind_speed = 0;
 const float pi = 3.1415;
+const float r = 0.0001;
 
+
+void isr() //interrupt service routine
+{
+rev++;
+}
 void setup() { 
   pinMode(outputA,INPUT);
   pinMode(outputB,INPUT);
   pinMode(button, INPUT);
-  pinMode(sensor,INPUT);
+  attachInterrupt(0,isr,RISING);  //attaching the interrupt
 
   Serial.begin(9600);
   // initial state of the outputA
@@ -53,33 +60,24 @@ void loop() {
   }
   direction();
   windspeed();    
-
   a_lastState = a_state; // Updates the previous state of the outputA with the current state
-
+  
   Serial.print(dir);
   Serial.print("  ***  ");
   Serial.println(wind_speed);  
   
-  
 }
 
-
 void windspeed(){
+  delay(1000);
+  detachInterrupt(0);           //detaches the interrupt
+  time=millis()-oldtime;        //finds the time 
+  rpm=(rev/time)*60000;         //calculates rpm
+  oldtime=millis();             //saves the current time
+  rev=0;
+  wind_speed = 2*pi*r*rpm*60;
+  attachInterrupt(0,isr,RISING);
 
-  // Wind Speed Calculation:
-  if(analogRead(sensor)<980)
-  {
-    time1 = millis();
-    delay(30);
-  }
-  if(analogRead(sensor)>980)
-  {
-    time2 = millis();
-    diff = (time2-time1); 
-    diff = diff / 1000;
-    wind_speed = (2 * pi * r * 3600)/diff;    
-  }
-  
 }
 void direction(){
 /*
